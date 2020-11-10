e07fa278bb5e0b37340f7deeaf9e4a104e0f050d
JSVargasQ <42383191+Sagrav10@users.noreply.github.com>
2020-11-09 21:51:19 -0500

    Update DataTable

--- Presentation/assets/scripts/common.js
+++ Presentation/assets/scripts/common.js
@@ -3,8 +3,8 @@
         $('.datatable-1').dataTable();
         $('.dataTables_paginate').addClass('btn-group datatable-pagination');
         $('.dataTables_paginate > a').wrapInner('<span />');
-        $('.dataTables_paginate > a:first-child').append('<i class="icon-chevron-left shaded"></i>');
-        $('.dataTables_paginate > a:last-child').append('<i class="icon-chevron-right shaded"></i>');
+        $('.dataTables_paginate > a:first-child').append('<i class="fa-chevron-left shaded"></i>');
+        $('.dataTables_paginate > a:last-child').append('<i class="fa-chevron-right shaded"></i>');
     
         $( '.slider-range').slider({
 			    range: true,
