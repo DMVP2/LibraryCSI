f2519d1860ecd1647837b5027b2544d4c485600d
JSVargasQ <42383191+Sagrav10@users.noreply.github.com>
2020-11-17 21:46:13 -0500

    Update Connection.php

--- Persistence/Connection.php
+++ Persistence/Connection.php
@@ -23,17 +23,17 @@ class Connection
      */
     public function connectBD()
     {
-        /* $server = "bookbosque.postgres.database.azure.com";
+        $server = "bookbosque.postgres.database.azure.com";
         $user = "bookbosque@bookbosque";
         $pass = "UEBbook1234";
         $bd = "book_bosque";
-        $port = "5432"; */
+        $port = "5432";
 
-        $server = "localhost";
+        /*$server = "localhost";
         $user = "postgres";
         $pass = "1234";
         $bd = "Book";
-        $port = "5432";
+        $port = "5432";*/
 
         $con = "host=$server port=$port dbname=$bd user=$user password=$pass";
         $connection = pg_connect($con);
