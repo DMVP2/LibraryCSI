631cf18ca58ceb9f2795737eed149b32185c9440
CarlosDavid07 <cdrodriguezc@unbosque.edu.co>
2020-11-23 18:31:57 -0500

    Create ModalMoreInfoDoc.php
    Creación modal de más infromación del documento

---
+++ Presentation/Fields/ModalMoreInfoDoc.php
@@ -0,0 +1,47 @@
+<?php
+
+include_once('../../../routes.php');
+
+include_once($_SERVER['DOCUMENT_ROOT'] . ROOT_DIRECTORY . ROUTE_SESSION . 'UserSession.php');
+
+include_once($_SERVER['DOCUMENT_ROOT'] . ROOT_DIRECTORY . ROUTE_DRIVINGS . 'DocumentDriving.php');
+
+include_once($_SERVER['DOCUMENT_ROOT'] . ROOT_DIRECTORY . ROUTE_PERSISTENCE . 'Connection.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . ROOT_DIRECTORY . ROUTE_ENTITIES . 'Document.php');
+$c = Connection::getInstance();
+$connection = $c->connectBD();
+$documentDriving = new DocumentDriving($connection);
+$documents = $documentDriving->listDocuments();
+$userSession = UserSession::getUserSession();
+
+$rol = $userSession->getRol();
+?>
+
+
+<!-- Modal -->
+<div class="modal fade modal-mini modal-primary" id="myModal1" tabindex="-1" role="dialog"
+    aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-dialog">
+            <div class="modal-content" style="margin-top:60%">
+
+                <div class="modal-header justify-content-center">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h5 class="modal-title" id="exampleModalLabel">
+                        <center>¿Que tipo de usuario deseas ser?</center>
+                    </h5>
+                </div>
+                <div class="modal-body text-center">
+                    <button type="button" class="btn btn-primary btn-outline-primary" style="width: 132px;"
+                        onclick="window.location.href='<?php echo ROOT_DIRECTORY . ROUTE_PRESENTATION . 'register.php?u=1' ?>'">Estandar</button>
+                    &nbsp;o&nbsp;
+                    <button type="button" class="btn btn-danger btn-outline-primary" style="width: 132px;"
+                        onclick="window.location.href='<?php echo ROOT_DIRECTORY . ROUTE_PRESENTATION . 'register.php?u=2' ?>'">Publicador</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
