14682eb1b2525e90f0eca8071d31ead43ba942f0
CarlosDavid07 <cdrodriguezc@unbosque.edu.co>
2020-11-27 13:30:53 -0500

    Revert "Revert "Cambios en el ver más de cada documento y detalles de vista""
    This reverts commit 4aa2d0640cdb2edfb7a5d8ae6349cb372033e0e3.

--- Presentation/Fields/ModalMoreInfoDoc.php
+++ Presentation/Fields/ModalMoreInfoDoc.php
@@ -17,6 +17,11 @@ $documentDriving = new DocumentDriving($connection);
 $document = $documentDriving->getDocument($_REQUEST['idDocument']);
 $authorsNames = $documentDriving->getAuthorsByDocumentId($_REQUEST['idDocument']);
 $publisherName = $documentDriving->getPublisherByDocumentId($_REQUEST['idDocument']);
+$cityCountry = $documentDriving->getCityCounty($_REQUEST['idDocument']);
+$queuesCount = $documentDriving->getQueuesCountByDocumentId($_REQUEST['idDocument']);
+
+//Variable del número de días máximos que puede tener en préstamo un libro un usuario
+$numDiasMaxPrestamo = 3;
 
 $idDoc = $_REQUEST['idDocument'];
 $digiFisi = $_REQUEST['digitalFisico'];
@@ -30,10 +35,10 @@ $digiFisi = $_REQUEST['digitalFisico'];
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
-        <h3 class="modal-title" id="exampleModalLongTitle">
+        <b><h3 class="modal-title" id="exampleModalLongTitle">
             <center> <?php echo $document->getTitle(); ?> — <?php echo $authorsNames[0]; ?></center>
 
-        </h3>
+        </h3></b>
     </div>
 
     <div class="row-md-12">
@@ -67,7 +72,10 @@ $digiFisi = $_REQUEST['digitalFisico'];
                     <b> No. Páginas:</b> <?php echo $document->getNumOfPages(); ?>
                 </p>
                 <p class="font-weight-light">
-                    <b>Lugar de Publicación: </b>xxxx<?php  ?>
+                    <b>Lugar de Publicación: </b><br><?php echo $cityCountry[0];  ?> — <?php echo $cityCountry[1];  ?>
+                </p>
+                <p class="font-weight-light">
+                    <b>Fecha: </b><?php echo $document ->getDateOfPublication();  ?><b style="font-size:xx-small;"> (AAAA-MM-DD)</b>
                 </p>
                 <p>
                     <?php
@@ -85,6 +93,22 @@ $digiFisi = $_REQUEST['digitalFisico'];
                 <p class="font-weight-light">
                     <b> Descripción:</b> <?php echo $document->getDescription(); ?>
                 </p>
+             
+                <?php if($queuesCount[0] > 0){ ?>
+                <p class="font-weight-light" style="color:darkcyan ;">
+                    <b> ¡Este libro se encuentra en préstamo!  </b>                       <br>
+                    El número de lectores en cola es <b><?php echo $queuesCount[0];?> </b>, tardará al rededor de <b><?php echo ( $queuesCount[0] * $numDiasMaxPrestamo ); ?> días.</b>
+                </p>
+                <?php } 
+                else{
+                    ?>
+                     <p class="font-weight-light" style="color:darkcyan ;">
+                    <b> ¡Este libro se encuentra en préstamo!  </b>                       <br>
+                Nadie ha ingresado a la cola, se el primero en hacerlo para acceder al libro en <b>máximo 3 días. </b>
+                </p>
+                <?php
+                }
+                ?>
 
             </div>
         </div>
@@ -93,7 +117,7 @@ $digiFisi = $_REQUEST['digitalFisico'];
 
                 <?php echo "<img src='" . ROOT_DIRECTORY . ROUTE_IMAGES . "documents/100anos.jpg" . "' style='width: 95%; height: 90%; margin-top:6%;'>"; ?>
                 <p class="font-weight-light">
-                <center><b> Publicador:</b> <?php echo $publisherName[0]; ?></center>
+                <center><i><b> Publicador:</b> <?php echo $publisherName[0]; ?></i></center>
                 </p>
 
 
